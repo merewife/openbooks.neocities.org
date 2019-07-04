6b106713f866772483b6ea8e320f2c5fd93bade2
merewife <merewife@protonmail.com>
2019-07-03 21:33:57 -0400

    updated index

---
+++ web/js/topbar.js
@@ -0,0 +1,1 @@
+document.write ('<div class="navbar"><ul><li><a href="https://openbooks.neocities.org/" class="title">OpenBooks 8 Series</a></li></ul><ul class="right"><li><a href="updates.html">Last updated: July 3, 2019</a></li></ul></div>');
