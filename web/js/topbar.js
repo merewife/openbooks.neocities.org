ed37431638f300e7806436fe5ea05af26beb6b87
merewife <merewife@protonmail.com>
2019-07-04 17:28:51 -0400

    updated version numbers

--- web/js/topbar.js
+++ web/js/topbar.js
@@ -1,1 +1,1 @@
-document.write ('<div class="navbar"><ul><li><a href="https://openbooks.neocities.org/" class="title">OpenBooks 8 Series</a></li></ul><ul class="right"><li><a href="updates.html">Last updated: July 3, 2019</a></li></ul></div>');
+document.write ('<div class="navbar"><ul><li><a href="https://openbooks.neocities.org/" class="title">OpenBooks 8 Series</a></li></ul><ul class="right"><li><a href="updates.html">Last updated: July 4, 2019</a></li></ul></div>');
