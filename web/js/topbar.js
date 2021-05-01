70ac10c194ce24df0f86162808cab4c8f2c33c40
Coyote Reyne <merewife@protonmail.com>
2021-05-01 12:21:01 -0400

    updated topbar JS

--- web/js/topbar.js
+++ web/js/topbar.js
@@ -1,1 +1,1 @@
-document.write ('<div class="navbar"><ul><li><a href="https://openbooks.neocities.org/" class="title">OpenBooks 8 Series</a></li></ul><ul class="right"><li><a href="updates.html">Last updated: May 1, 2021</a></li></ul></div>');
+document.write ('<div class="navbar"><ul><li><a href="https://openbooks.neocities.org/" class="title">Welcome to OpenBooks</a></li></ul><ul class="right"><li><a href="updates.html">Last updated: May 1, 2021</a></li></ul></div>');
