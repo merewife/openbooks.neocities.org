0f4920b5837760634620789b1fae11fa25ddefc3
Coyote Reyne <merewife@protonmail.com>
2021-05-11 16:15:55 -0400

    updates

---
+++ web/js/statusbar.js
@@ -0,0 +1,1 @@
+document.write ('<div class="statusbar"><ul><li><a href="https://openbooks.neocities.org/" class="title">OpenBooks 9 Series</a></li></ul><ul class="right"><li><a href="updates.html">Last updated on October 24, 2021</a></li></ul></div>');
