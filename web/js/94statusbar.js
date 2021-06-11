64b3229a618b93b10002e5690ab57bedc5947f18
Coyote Reyne <merewife@protonmail.com>
2021-06-10 21:42:41 -0400

    load handler improved and moved to core pages

---
+++ web/js/94statusbar.js
@@ -0,0 +1,1 @@
+document.write ('<div class="statusbar"><ul><li class="logo"><a href="https://openbooks.neocities.org/">OpenBooks</a></li><li class="spinner"></li><li class="indicator">Please wait...</li></li></ul><ul class="right"><li><a href="updates.html">Last updated on June 10, 2021</a></li></ul></div>');
