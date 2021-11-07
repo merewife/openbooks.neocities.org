f4e3ea30779def2f34171d5ff45e7f59198c5d03
Coyote Reyne <merewife@protonmail.com>
2021-11-07 10:11:18 -0500

    updates

--- web/js/96statusbar.js
+++ web/js/96statusbar.js
@@ -1,1 +1,1 @@
-document.write ('<a class="white" href="/updates.html"><li>Last updated on November 2, 2021</li></a>');
+document.write ('<a class="white" href="/updates.html"><li>Last updated on November 7, 2021</li></a>');
