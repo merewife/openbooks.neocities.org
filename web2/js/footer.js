17ab7f1d4068feffe955a6c98e6b8c54c17b2e06
Coyote Reyne <chromebooks@aol.com>
2024-01-01 19:50:11 -0500

    footer

---
+++ web2/js/footer.js
@@ -0,0 +1,1 @@
+document.write ('<div class="footer"><div class="row"><div class="col"><a href="/license.txt">Licensed under MIT</a> &bull; &copy; 2014-2024 &bull; <a href="/about">About OpenBooks</a></div><div class="col right"><a href="/about/updates">Last update: January 1st, 2024</a></div></div></div>');
